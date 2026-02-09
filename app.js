65be86597722e3a29da3f2a0168225ec6e989504
Manas jha <manasjha2233@gmail.com>
2026-02-09 20:34:42 +0530

    Fix production root route and stabilize deployment setup

--- app.js
+++ app.js
@@ -89,6 +89,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 
 app.use((req, res, next) => {
